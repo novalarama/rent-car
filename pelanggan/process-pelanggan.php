50ee773d61c8dd8b4e12ebb9d8353346b2fb1d03
Noval AKbar Ramadhany <novalakbarramadhany@gmail.com>
2021-11-01 10:53:26 +0700

    Rent-Car

---
+++ pelanggan/process-pelanggan.php
@@ -0,0 +1,45 @@
+<?php
+include("../connection.php");
+
+if (isset($_POST["simpan_pelanggan"])) {
+    // tampung data input anggota dari user
+    
+    $nama_pelanggan = $_POST["nama_pelanggan"];
+    $alamat_pelanggan = $_POST["alamat_pelanggan"];
+    $kontak = $_POST["kontak"];
+
+    //membuat perintah sql untuk insert data ke table anggota
+    $sql = "insert into pelanggan values
+    ('','$nama_pelanggan','$alamat_pelanggan','$kontak')";
+
+    //eksekusi perintah sql
+    $tambah = mysqli_query($connect, $sql);
+
+    //direct ke halaman list-anggota
+    if ($tambah) {
+        header('Location:list-pelanggan.php');
+    } else {
+        printf('Gagal'.mysqli_error($connect));
+        exit();
+    }
+
+# untuk update
+
+}else if(isset($_POST["edit_pelanggan"])){
+        # menampung dulu data yang akan di update
+        $id_pelanggan = $_POST["id_pelanggan"];
+        $nama_pelanggan = $_POST["nama_pelanggan"];
+        $alamat_pelanggan = $_POST["alamat_pelanggan"];
+        $kontak = $_POST["kontak"];
+        
+        $edit = mysqli_query($connect, $sql);
+        
+        if ($edit) {
+            header('Location:list-pelanggan.php');
+        } else {
+            printf('Gagal'.mysqli_error($connect));
+            exit();
+        }
+        
+}
+?>
---
+++ sewa/process-sewa.php
@@ -0,0 +1,36 @@
+<?php
+include("../connection.php");
+# menampung data yang dikirim
+$id_sewa = $_POST["id_sewa"];
+$tgl_sewa = $_POST["tgl_sewa"];
+$id_pelanggan = $_POST["id_pelanggan"];
+$id_karyawan = $_POST["id_karyawan"];
+$total_bayar = $_POST["total_bayar"]; 
+$mobil = $_POST["id_mobil"]; //array
+
+# perintah SQL untuk insert ke table sewa
+$sql = "insert into sewa values
+('','$id_karyawan','$id_pelanggan','$tgl_sewa','$total_bayar')";
+
+if (mysqli_query($connect, $sql)) {
+    # jika berhasil insert ke tabel sewa
+    # insert ke tabel detail sewa
+    for ($i=0; $i < count($mobil); $i++) { 
+        $id_mobil = $mobil[$i];
+        $sql = "insert into detail_sewa values
+        ('$id_sewa','$id_mobil')";
+        if (mysqli_query($connect, $sql)) {
+            # jika berhasil insert ke table detail_sewa
+
+        }else {
+            # jika gaga insert ke table detail_sewa
+            echo mysqli_error($connect);
+            exit;
+        }
+    }
+    header('Location:list-sewa.php');
+}else{
+    # jia gagal insert tabel sewa
+    echo mysqli_error($connect);
+}
+?>
